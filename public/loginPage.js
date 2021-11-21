a3cc77591702fb7f6b55b112875c4dfe09e317ff
Stefania <stt.orlova@gmail.com>
2021-11-21 12:47:26 +0300

    task

---
+++ public/loginPage.js
@@ -0,0 +1,15 @@
+'Use strict'
+
+const userForm = new UserForm();
+
+userForm.loginFormCallback = data => {
+    ApiConnector.login(data, response => {
+        response.success ? location.reload() : userForm.setLoginErrorMessage(response.error);
+    });
+};
+
+userForm.registerFormCallback = data => {
+    ApiConnector.register(data, response => {
+        response.success ? location.reload() : userForm.setRefisterErrorMessage(response.error);
+    });
+};
