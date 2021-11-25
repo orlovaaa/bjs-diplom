42de0f365c57aaaa18b8578af66dfa38a35343a1
Stefania <stt.orlova@gmail.com>
2021-11-25 11:51:57 +0300

    task

--- public/homePage.js
+++ public/homePage.js
@@ -29,3 +29,73 @@ function api() {
 
 api()
 setInterval(api, 60000);
+
+//--Операции с деньгами--
+const moneyManager = new MoneyManager();
+
+//Пополнение баланса
+moneyManager.addMoneyCallback = data => {
+    ApiConnector.addMoney(data, response => {
+        if (response.success) {
+            ProfileWidget.showProfile(response.data)
+        }
+        moneyManager.setMessage(response.success, response.success ? "Баланс пополнен!" : response.error);
+    })
+};
+
+
+//Конвертирование валюты
+moneyManager.conversionMoneyCallback = data => {
+    ApiConnector.convertMoney(data, response => {
+        if (response.success) {
+            ProfileWidget.showProfile(response.data)
+        }
+        moneyManager.setMessage(response.success, response.success ? "Конвертироно успешно!" : response.error)
+    })
+};
+
+//Перевод валюты
+moneyManager.sendMoneyCallback = data => {
+    ApiConnector.transferMoney(data, response => {
+        if (response.success) {
+            ProfileWidget.showProfile(response.data)
+        }
+        moneyManager.setMessage(response.success, response.success ? "Перевод выполнен!" : response.error)
+    })
+};
+
+//--Работа с избранным--
+const favoritesWidget = new FavoritesWidget()
+
+//Начальный список избранного
+ApiConnector.getFavorites((data) => {
+    if (data.success) {
+        favoritesWidget.clearTable();
+        favoritesWidget.fillTable(data.data);
+        moneyManager.updateUsersList(data.data);
+    }
+});
+
+//Добавление в избранное
+favoritesWidget.addUserCallback = (data) => {
+    ApiConnector.addUserToFavorites(data, response => {
+        if (response.success) {
+            favoritesWidget.clearTable()
+            favoritesWidget.fillTable(response.data);
+            moneyManager.updateUsersList(response.data);
+        }
+        moneyManager.setMessage(response.success, response.success ? "Пользователь добавлен!" : response.error)
+    })
+};
+
+//Удаление из избранного
+favoritesWidget.removeUserCallback = (data) => {
+    ApiConnector.removeUserFromFavorites(data, response => {
+        if (response.success) {
+            favoritesWidget.clearTable()
+            favoritesWidget.fillTable(response.data);
+            moneyManager.updateUsersList(response.data);
+        }
+        moneyManager.setMessage(response.success, response.success ? "Пользователь удален!" : response.error)
+    })
+};
